1f4073b29156833a88e1115e40d131464123f564
katahirado <yuichi@katahirado.jp>
2014-03-18 11:13:00 +0900

    first commit turbolikns off

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kkfoodstuff::Application.config.secret_key_base = '9a2923f65862b82df6e475f3fadb1afea371a6fed168a62d1dac4020c5b2888663386fd06ad04d5895a1af59deded6fad14bef7db40971b3efe96360a3730c22'
