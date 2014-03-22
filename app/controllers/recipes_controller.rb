4d43195d3f79ac0c627fedb1fd8d83b9af36a18c
katahirado <yuichi@katahirado.jp>
2014-03-23 03:10:18 +0900

    url -> path

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,7 +20,7 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to recipes_url, notice: 'Recipe was successfully created.' }
+        format.html { redirect_to recipes_path, notice: 'Recipe was successfully created.' }
         format.json { render action: 'show', status: :created, location: @recipe }
       else
         format.html { render action: 'new' }
@@ -32,7 +32,7 @@ class RecipesController < ApplicationController
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
-        format.html { redirect_to recipes_url, notice: 'Recipe was successfully updated.' }
+        format.html { redirect_to recipes_path, notice: 'Recipe was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -44,7 +44,7 @@ class RecipesController < ApplicationController
   def destroy
     @recipe.destroy
     respond_to do |format|
-      format.html { redirect_to recipes_url }
+      format.html { redirect_to recipes_path }
       format.json { head :no_content }
     end
   end
