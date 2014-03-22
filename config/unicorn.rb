251ecd660e336b18e8cc633597acfeae5b96ec49
katahirado <yuichi@katahirado.jp>
2014-03-23 02:40:43 +0900

    modified:   config/unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -101,17 +101,15 @@
   # correctly implements pread()/pwrite() system calls)
 #end
 
-# ワーカーの数
 worker_processes 2
 
-# ソケット経由で通信する
+user "_www", "_www"
+
 listen File.expand_path('tmp/sockets/unicorn.sock', ENV['RAILS_ROOT'])
 
-# ログ
 stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
 stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
 
-# ダウンタイムなくす
 preload_app true
 
 before_fork do |server, worker|
@@ -120,7 +118,6 @@ before_fork do |server, worker|
   old_pid = "#{ server.config[:pid] }.oldbin"
   unless old_pid == server.pid
     begin
-      # SIGTTOU だと worker_processes が多いときおかしい気がする
       Process.kill :QUIT, File.read(old_pid).to_i
     rescue Errno::ENOENT, Errno::ESRCH
     end
