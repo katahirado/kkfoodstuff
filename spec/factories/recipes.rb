380ed96d3405c7978d88b338104f39dab366eff3
katahirado <yuichi@katahirado.jp>
2014-03-24 19:02:38 +0900

    modified test
    	modified:   spec/controllers/recipes_controller_spec.rb
    	modified:   spec/factories/recipes.rb
    	modified:   spec/factories/search_contents.rb

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,5 +1,3 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :recipe do
     title "MyString"
--- spec/factories/search_contents.rb
+++ spec/factories/search_contents.rb
@@ -1,7 +1,6 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :search_content do
-    recipe nil
+    origin_title "MyString"
+    origin_content "MyText"
   end
 end
