87b8906b4e24f1985a5c910b3ad7e22caf0d3cc4
katahirado <yuichi@katahirado.jp>
2014-03-31 00:04:01 +0900

    added searches_controller test

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -3,9 +3,24 @@ require 'spec_helper'
 describe SearchesController do
 
   describe "GET 'index'" do
+    before(:all) do
+      DatabaseCleaner.strategy = :truncation
+    end
+
+    after(:all) do
+      DatabaseCleaner.strategy = :transaction
+    end
+
     it "returns http success" do
-      get 'index'
+      get :index
       expect(response).to be_success
+    end
+
+    it "assigns search result recipes as @recipes" do
+      word = '白菜'
+      recipe = FactoryGirl.create(:recipe, title: word)
+      get :index,search: { word: word}
+      expect(assigns(:recipes)).to eq([recipe])
     end
   end
 
