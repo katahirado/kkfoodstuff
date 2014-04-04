b5caf2fa805ee36cfca3d7218e950a375bc28fa7
katahirado <yuichi@katahirado.jp>
2014-04-04 20:39:43 +0900

    fix migration

--- db/migrate/20140404020641_add_title_yomi_to_search_content.rb
+++ db/migrate/20140404020641_add_title_yomi_to_search_content.rb
@@ -1,9 +1,6 @@
 class AddTitleYomiToSearchContent < ActiveRecord::Migration
   def self.up
     add_column :search_contents, :title_yomi, :string
-    SearchContent.all.each do |sc|
-      sc.update(title_yomi: Analyze.parse(sc.origin_title, '-Oyomi'))
-    end
   end
 
   def self.down
--- db/migrate/20140404045945_add_title_ngram_and_content_ngram_to_search_content.rb
+++ db/migrate/20140404045945_add_title_ngram_and_content_ngram_to_search_content.rb
@@ -3,7 +3,9 @@ class AddTitleNgramAndContentNgramToSearchContent < ActiveRecord::Migration
     add_column :search_contents, :title_ngram, :string
     add_column :search_contents, :content_ngram, :text
     SearchContent.all.each do |sc|
-      sc.update(title_ngram: Analyze.ngram(sc.origin_title), content_ngram: Analyze.ngram(sc.origin_content))
+      sc.update(title_yomi: Analyze.parse(sc.origin_title, '-Oyomi'),
+                title_ngram: Analyze.ngram(sc.origin_title),
+                content_ngram: Analyze.ngram(sc.origin_content))
     end
   end
 
