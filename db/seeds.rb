44c67b651a2f1516df6124d0318a04edee02bf77
katahirado <yuichi@katahirado.jp>
2014-04-16 16:19:10 +0900

    indent

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Recipe.delete_all
 
- Recipe.create( title: '豚肉と白菜の炒めもの', content: "人参\n豚肉\n白菜", book: '4月号 50P') 
- Recipe.create( title: 'ハクサイの炒めもの', content: "にんじん\n豚肉\n白菜", book: '5月号 50P') 
- Recipe.create( title: '炒めもの', content: "ニンジン\n豚肉\n白菜", book: '6月号 50P') 
- Recipe.create( title: '麩の吸いもの', content: "麩", book: '7月号 50P') 
- Recipe.create( title: '麩の卵とじ', content: "麩\n卵", book: '8月号 50P') 
+Recipe.create(title: '豚肉と白菜の炒めもの', content: "人参\n豚肉\n白菜", book: '4月号 50P')
+Recipe.create(title: 'ハクサイの炒めもの', content: "にんじん\n豚肉\n白菜", book: '5月号 50P')
+Recipe.create(title: '炒めもの', content: "ニンジン\n豚肉\n白菜", book: '6月号 50P')
+Recipe.create(title: '麩の吸いもの', content: "麩", book: '7月号 50P')
+Recipe.create(title: '麩の卵とじ', content: "麩\n卵", book: '8月号 50P') 
